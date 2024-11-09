2f221cd9567826727f945875c3dc490c2798cbb5
BilalMP <peeroo270@gmail.com>
2024-11-09 23:07:07 +0400

    Hire Pro

---
+++ src/components/HirePro.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Container from './Container'
+
+const HirePro = () => {
+    return (
+        <Container>
+            <div className="flex justify-start gap-10 mt-5 px-10">
+                <h1 className='capitalize font-bold text-2xl mt-3'>DreamHost</h1>
+                <h1 className='capitalize font-light text-2xl mt-3'>Pro Services</h1>
+            </div>
+            <div className="px-10 mt-[200px]">
+                <p className='text-3xl font-bold'>Hire a Pro, We'll Do <br />the Work for You</p>
+            </div>
+        </Container>
+    )
+}
+// public\Hirepro.jpg
+
+export default HirePro
