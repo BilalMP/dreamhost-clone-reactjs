65b41dcaf33b2434fbb527e98d51aa0ad144512e
BilalMP <peeroo270@gmail.com>
2024-11-13 21:52:17 +0400

    Hosting Purpose

---
+++ src/components/HostingPurpose.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Container from './Container'
+
+const HostingPurpose = () => {
+    return (
+        <Container>
+            <div className="h-[400px] flex justify-start items-center">
+                <p className='text-6xl font-bold w-1/2 mx-[50px]'>We make sure your purpose is our purpose</p>
+            </div>
+        </Container>
+    )
+}
+
+export default HostingPurpose
